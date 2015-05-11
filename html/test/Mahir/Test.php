5a2557aeaabb84da2349703e2823ef885aa1c6f1
Mahir Morshed <mahir.morshed@gmail.com>
2015-05-11 00:42:20 +0000

    Added test

---
+++ html/test/Mahir/Test.php
@@ -0,0 +1,12 @@
+ <?
+ include _once('/var/www/html/src/php/CDPriorityQueve.php');
+
+ $PQ = new CDPriorityQueve();
+ $PQ -> inser + ('A',1);
+ $PQ -> inser + ('B',4);
+ $PQ -> inser + ('C',2);
+ $PQ -> inser + ('D',3);
+ $PQ -> inser + ('F',10);
+ echo PQ -> top();
+
+?>
